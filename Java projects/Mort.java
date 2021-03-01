69ddb41fbd73d44306891628408e9319fe25a976
sahilhd <76663713+sahilhd@users.noreply.github.com>
2021-02-28 19:07:36 -0500

    Add files via upload

---
+++ Java projects/Mort.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        // if divible by 5 = fizz
+        // if divible by 3 == buzz
+        // if both then fizzbuzz
+        // else just the number
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if( number % 5 == 0 & number % 3 ==0){
+            System.out.println("FizzBuzz");
+        }
+        else if (number % 5 == 0){
+            System.out.println("Fizz");
+        }
+        else if (number % 3 == 0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(number);
+        }
+
+
+
+
+
+
+    }
+}
