33b01908a116d262d3de042e2a4a4367fd2a694d
sahilhd <76663713+sahilhd@users.noreply.github.com>
2021-03-29 01:24:05 -0400

    Add files via upload

---
+++ Breadth First Search Algo/AppBFS.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+//import "./App.css";
+
+import React from "react";
+import BFS from "./BFS/BFS";
+
+function App() {
+  return (
+    <div className="App">
+      <BFS></BFS>
+    </div>
+  );
+}
+
+export default App;
