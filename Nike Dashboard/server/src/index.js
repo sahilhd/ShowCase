734acb4ecb1f70734dbb433c91846bc889aa29b5
Sahil Handa <sahilhandauni@gmail.com>
2023-09-24 16:17:24 -0400

    shoe project dash

---
+++ Nike Dashboard/server/src/index.js
@@ -0,0 +1,15 @@
+//const express = require("express")
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { userRouter} from './routes/users.js'
+//remove password
+const app = express()
+
+app.use(express.json()); // sending data from the front end 
+app.use(cors());
+app.use("/auth", userRouter);
+
+mongoose.connect("mongodb+srv://sahilhandauni:--@shoes.mfn7pp7.mongodb.net/---?retryWrites=true&w=majority")
+app.listen(3001, () => console.log("START")); // start api on port 3001
+
